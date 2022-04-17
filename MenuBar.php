803b80fc80bd902a78147d08c91b88e8f6dea5f2
jzagame <67995901+jzagame@users.noreply.github.com>
2022-04-17 15:22:28 +0800

    menubar.php provided

---
+++ MenuBar.php
@@ -0,0 +1,45 @@
+<script src="Bootstrap4.6.1/jquery3.5.1.slim.min.js"></script>
+<script src="Bootstrap4.6.1/bootstrap4.6.1.min.js"></script>
+<script src="Bootstrap4.6.1/popper1.6.1.min.js"></script>
+<link rel="stylesheet" href="Bootstrap4.6.1/bootstrap4.6.1.css"/>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+        <div class="dropdown"> <!-- Start  -->
+            <div class="btn dropdown-toggle" data-toggle="dropdown" id="id1" aria-hospopup="true" aria-expanded="false"> <!-- remember to change id name, both 2 must same -->
+                Learning
+            </div>
+            <div class="dropdown-menu" aria-labelledby="id1"> <!-- remember to change id name, both 2 must same -->
+                <a class="dropdown-item" href="#">asdasds</a>
+                <a class="dropdown-item" href="#">asdasds</a>
+                <a class="dropdown-item" href="#">asdasds</a>
+            </div>
+        </div> <!-- End if want add more menubar , just copy code from start to end -->
+        <div class="dropdown">
+            <div class="btn dropdown-toggle" data-toggle="dropdown" id="id2" aria-hospopup="true" aria-expanded="false">
+                Learning
+            </div>
+            <div class="dropdown-menu" aria-labelledby="id2">
+                <a class="dropdown-item" href="#">asdasds</a>
+                <a class="dropdown-item" href="#">asdasds</a>
+                <a class="dropdown-item" href="#">asdasds</a>
+            </div>
+        </div>
+        <div class="dropdown">
+            <div class="btn dropdown-toggle" data-toggle="dropdown" id="Learning" aria-hospopup="true" aria-expanded="false">
+                Learning
+            </div>
+            <div class="dropdown-menu" aria-labelledby="Learning">
+                <a class="dropdown-item" href="#">asdasds</a>
+                <a class="dropdown-item" href="#">asdasds</a>
+                <a class="dropdown-item" href="#">asdasds</a>
+            </div>
+        </div>
+    </div>
+  </div>
+</nav>
