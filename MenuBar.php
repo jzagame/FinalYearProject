14e6f2d8cead78d3c993fcf2c51a5b01c50c5a94
Lu Si Wei <102765796@students.swinburne.edu.my>
2022-04-20 16:41:29 +0800

    Update Title

--- MenuBar.php
+++ MenuBar.php
@@ -8,7 +8,7 @@
   <div class="row">
     <div class="justify-content-center">
       <nav class="navbar navbar-expand-lg navbar-light bg-ligh">
-        <a class="navbar-brand" href="index.php">LONGI</a>
+        <a class="navbar-brand" href="index.php">LONGi</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
